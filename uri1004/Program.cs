dd25f3269bd5d93ee3fbf12be1f45f9e92c9a472
Diego Pereira Salles <diego.salles@wooza.com.br>
2020-04-27 22:24:54 -0300

    Exercicio URI 1004

---
+++ uri1004/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace uri1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, prod;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            prod = a * b;
+
+            Console.WriteLine("PROD = " + prod);
+        }
+    }
+}
