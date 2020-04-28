5f68c0a57d7e43ed198829a196e83a26d960a68f
Diego Pereira Salles <diego.salles@wooza.com.br>
2020-04-27 22:45:25 -0300

    Exercicio URI 1005 e URI 1006

---
+++ uri1005/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, media;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = ((a * 3.5) + (b * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
