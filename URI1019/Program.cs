fe42a1acd94346ac72c159bb4ba5aaf48a98d003
Diego Pereira Salles <diego.salles@wooza.com.br>
2020-05-18 20:20:35 -0300

    Exercicio URI 1019

---
+++ URI1019/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace URI1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, Horas, Resto, Minutos, Segundos;
+
+            N = int.Parse(Console.ReadLine());
+
+            Horas = N / 3600;
+            Resto = N % 3600;
+            Minutos = Resto / 60;
+            Segundos = Resto % 60;
+
+            Console.WriteLine(Horas + ":" + Minutos + ":" + Segundos);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
